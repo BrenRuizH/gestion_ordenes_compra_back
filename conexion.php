49d168670f6be1200c30938da8b8c73f420297b4
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-04-02 12:00:02 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysql = new mysqli("o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ie5g1p0as6ry8me4", "cd1w2cpo2o5c94fa", "xj9i40s68gou6ok7");
+$mysql = new mysqli_connect("o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ie5g1p0as6ry8me4", "cd1w2cpo2o5c94fa", "xj9i40s68gou6ok7");
 
 if ($mysql->connect_error) {
     die("Error de conexión");
