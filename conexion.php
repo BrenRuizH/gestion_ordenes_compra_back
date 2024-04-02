2b2ed7e4f572f01bb7c0c41c16712899b6626de2
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-04-02 12:15:08 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 
-$mysql = mysqli_connect("localhost", "root", "root", "bd_distribuidora");
+$mysql = new mysqli("localhost", "root", "root", "bd_distribuidora");
 
 if ($mysql->connect_error) {
     die("Error de conexión");
-} 
+}
