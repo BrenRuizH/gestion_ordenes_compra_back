2f0ea4af0d60aa3576cfe4254d59de71ae29f6f7
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-04-02 12:28:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,32 @@
 <?php
-require_once 'conexion.php';
-include 'config.php';
+if($_SERVER["REQUEST_METHOD"]=="GET")
+{
+    require_once 'conexion.php';
+    include 'config.php';
 
-$query = $mysql->query("SELECT * FROM usuarios");
-
-$usuarios = array();
-while ($row = $query->fetch_assoc()) {
-    $usuarios[] = $row;
+    $query="SELECT usuarios.id, usuarios.nombre, usuarios.contrasenia, roles.nombre AS rol 
+            FROM usuarios 
+            INNER JOIN roles ON usuarios.rol_id = roles.id";
+    $resultado=$mysql->query($query);
+    if($resultado->num_rows > 0)
+    {
+        $itemRecords=array();
+        $itemRecords["items"]=array();
+        while ($item = $resultado->fetch_assoc())
+        {
+            extract($item);
+            $itemDetails=array(
+                "id" => $id,
+                "nombre" => $nombre,
+                "contrasenia" => $contrasenia,
+                "rol" => $rol
+            );
+            array_push($itemRecords["items"], $itemDetails);
+        }
+        http_response_code(200);
+        echo json_encode($itemRecords);
+    } else {
+        http_response_code(404);
+        echo json_encode(array("message" => "No se encontraron usuarios."));
+    }
 }
-
-$json_result = json_encode($usuarios);
-
-echo $json_result;
-?>
