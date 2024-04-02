fc3f728de734c5e909ec11a4cd7b483f1d6b13f0
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-04-01 22:12:22 -0600

    Delete conexion.php

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$mysql = new mysqli("localhost", "root", "root", "bd_distribuidora");
-
-if ($mysql->connect_error) {
-    die("Error de conexión");
-} else {
-   // echo "Conexión exitosa";
-} 
