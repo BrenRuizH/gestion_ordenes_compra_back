cd71c50356202036000ac6cc5d05734ed2eff38c
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-04-14 19:37:09 -0600

    Update editar.php

--- hormas/editar.php
+++ hormas/editar.php
@@ -7,8 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
     $nombre = $_POST['nombre'];
     $cliente_id = $_POST['cliente_id'];
+    $matriz = $_POT['matriz'];
+    $cambrillon = $_POT['cambrillon'];
+    $materiales = $_POT['materiales'];
+    $observaciones = $_POT['observaciones'];
 
-    $query = "CALL EditarHorma($id, '$nombre', $cliente_id);";
+    $query = "CALL EditarHorma($id, '$nombre', $cliente_id, $matriz, '$cambrillon', '$materiales', '$observaciones');";
 
     $resultSet = $mysql->query($query);
     if(!$resultSet){
