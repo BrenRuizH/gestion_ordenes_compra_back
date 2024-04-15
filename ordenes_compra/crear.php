223f73726df879595c8863549b024ff3acebee45
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-04-14 19:50:30 -0600

    Update crear.php

--- ordenes_compra/crear.php
+++ ordenes_compra/crear.php
@@ -11,9 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $folio = $_POST['folio'];
     $orden_compra_c = $_POST['orden_compra_c'];
     $horma_id = $_POST['horma_id'];
-    $cambrillon_id = isset($_POST['cambrillon_id']) && $_POST['cambrillon_id'] !== '' ? $_POST['cambrillon_id'] : NULL;
-    $observaciones = isset($_POST['observaciones']) ? $_POST['observaciones'] : NULL;
-    $matriz = isset($_POST['matriz']) ? $_POST['matriz'] : NULL;
     $total_pares = $_POST['total_pares'];
     $punto = array_map('floatval', explode(',', $_POST['punto']));
     $cantidad = array_map('intval', explode(',', $_POST['cantidad']));
@@ -21,8 +18,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     try {
         $mysql->begin_transaction();
 
-        $stmt = $mysql->prepare("CALL CrearOrdenCompra(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssissiissi", $fecha_orden, $fecha_entrega, $cliente_id, $folio, $orden_compra_c, $horma_id, $cambrillon_id, $observaciones, $matriz, $total_pares);
+        $stmt = $mysql->prepare("CALL CrearOrdenCompra(?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssissii", $fecha_orden, $fecha_entrega, $cliente_id, $folio, $orden_compra_c, $horma_id, $total_pares);
         if (!$stmt->execute()) {
             throw new Exception("Error al insertar orden de compra: " . $stmt->error);
         }
@@ -47,22 +44,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
 
-        if (isset($_POST['material_id']) && $_POST['material_id'] !== '') {
-            $material_id = explode(',', $_POST['material_id']);
-            $stmt = $mysql->prepare("INSERT INTO materiales_orden_compra(orden_compra_id, material_id) VALUES (?, ?)");
-
-            if (is_array($material_id)) {
-                foreach ($material_id as $mid) {
-                    $stmt->bind_param("ii", $orden_compra_id, $mid);
-                    if (!$stmt->execute()) {
-                        throw new Exception("Error al insertar material de orden de compra: " . $stmt->error);
-                    }
-                }
-            } else {
-                throw new Exception("El material no es un array");
-            }
-        }
-        
         if ($mysql->commit()) {
             echo json_encode(["status"=>"success","message" => "Orden de compra creada exitosamente"]);
         } else {
