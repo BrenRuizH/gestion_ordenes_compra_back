841732f66207201f84a5049f933f6c612b7fb659
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-04-16 15:25:09 -0600

    Update consulta.php

--- ordenes_compra/consulta.php
+++ ordenes_compra/consulta.php
@@ -17,26 +17,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         if ($orden) {
             $orden_compra_c = $orden['orden_compra_c'];
 
-            // Extrae el prefijo y el número de orden_compra_c
             $prefix = substr($orden_compra_c, 0, 3);
             $number = intval(substr($orden_compra_c, 3));
 
-            // Incrementa el número en uno
             $number++;
 
-            // Concatena el prefijo y el número para obtener el nuevo valor de orden_compra_c
             $new_orden_compra_c = $prefix . $number;
         } else {
-            // Si es la primera orden del cliente, genera el orden_compra_c con las primeras 3 letras del código del cliente y el número 1
-            $query = "SELECT codigo FROM clientes WHERE id = ?";
+            $query = "SELECT acronimo FROM clientes WHERE id = ?";
             $stmt = $mysql->prepare($query);
             $stmt->bind_param("i", $cliente_id);
             $stmt->execute();
             $result = $stmt->get_result();
             $cliente = $result->fetch_assoc();
-            $codigo_cliente = $cliente['codigo'];
+            $acronimo_cliente = $cliente['acronimo'];
 
-            $new_orden_compra_c = substr($codigo_cliente, 0, 3) . '1';
+            $new_orden_compra_c = $acronimo_cliente . '1';
         }
 
         echo json_encode(["new_orden_compra_c" => $new_orden_compra_c]);
