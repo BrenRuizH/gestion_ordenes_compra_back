9fe10adf30c170b766fbedb736fe9eff7b9e2a34
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-06-04 15:37:48 -0600

    Agregar a la vista el No. Factura

--- ordenes_compra/leer.php
+++ ordenes_compra/leer.php
@@ -5,7 +5,7 @@ if($_SERVER["REQUEST_METHOD"]=="GET")
     require_once '../conexion.php';
     include '../config.php';
 
-    $query="SELECT oc.id, oc.folio, c.codigo, oc.orden_compra_c, oc.fecha_orden, oc.fecha_entrega, oc.total_pares 
+    $query="SELECT oc.id, oc.folio, c.codigo, oc.orden_compra_c, oc.fecha_orden, oc.fecha_entrega, oc.total_pares, oc.facturaNo
             FROM ordenes_compra oc 
             INNER JOIN clientes c ON oc.cliente_id = c.id
             ORDER BY oc.fecha_orden DESC, oc.folio DESC;";
