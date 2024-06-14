f2c2f60c8e2b3ad5a317e3a3749af124a7f87dc8
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-06-14 10:02:40 -0600

    Actualización servicio reporte.php

--- ordenes_compra/reporte.php
+++ ordenes_compra/reporte.php
@@ -7,44 +7,45 @@ if($_SERVER["REQUEST_METHOD"]=="GET")
 
     $fecha_inicio = $_GET['fecha_inicio'];
     $fecha_fin = $_GET['fecha_fin'];
+
+    try {
     
-    $query="SELECT oc.id as orden_id, oc.folio, c.codigo, oc.orden_compra_c, oc.fecha_orden, oc.fecha_entrega, oc.total_pares, oc.facturaNo, oc.status  
+        $stmt = $mysql -> prepare("SELECT oc.id as orden_id, oc.folio, c.codigo, oc.orden_compra_c, oc.fecha_orden, oc.fecha_entrega, oc.total_pares, oc.facturaNo, oc.status  
             FROM ordenes_compra oc 
             INNER JOIN clientes c ON oc.cliente_id = c.id
-            WHERE oc.fecha_orden BETWEEN '$fecha_inicio' AND '$fecha_fin'
-            ORDER BY oc.fecha_orden DESC, oc.folio DESC;";
-    
+            WHERE oc.fecha_orden BETWEEN ? AND ?
+            ORDER BY oc.fecha_orden DESC, oc.folio DESC;");
 
-    $resultado=$mysql->query($query);
-    if($resultado->num_rows > 0) {
-        $itemRecords=array();
-        $itemRecords["items"]=array();
-        while ($item = $resultado->fetch_assoc()) {
-            extract($item);
+        $stmt->bind_param("ss", $fechaInicio, $fechaFin);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+
+        if($resultado->num_rows > 0) {
+            $itemRecords=array();
+            $itemRecords["items"]=array();
+            while ($item = $resultado->fetch_assoc()) {
+                extract($item);
             
-            $itemDetails=array(
-                "id" => $orden_id,
-                "folio" => $folio,
-                "codigo" => $codigo,
-                "orden_compra_c" => $orden_compra_c,
-                "fecha_orden" => $fecha_orden,
-                "fecha_entrega" => $fecha_entrega,
-                "total_pares" => $total_pares,
-                "facturaNo" => $facturaNo,
-                "status" => $status
-            );
-            array_push($itemRecords["items"], $itemDetails);
-        }
-        
-        $queryFolioMax="SELECT MAX(folio) as maxFolio FROM ordenes_compra";
-        $resultadoFolioMax=$mysql->query($queryFolioMax);
-        $folioMax = $resultadoFolioMax->fetch_assoc();
-        $itemRecords["maxFolio"] = $folioMax['maxFolio'];
+                $itemDetails=array(
+                    "id" => $orden_id,
+                    "folio" => $folio,
+                    "codigo" => $codigo,
+                    "orden_compra_c" => $orden_compra_c,
+                    "fecha_orden" => $fecha_orden,
+                    "fecha_entrega" => $fecha_entrega,
+                    "total_pares" => $total_pares,
+                    "facturaNo" => $facturaNo,
+                    "status" => $status
+                );
+                array_push($itemRecords["items"], $itemDetails);
+            }
 
-        http_response_code(200);
-        echo json_encode($itemRecords);
-    } else {
-        http_response_code(404);
-        echo json_encode(array("message" => "No se encontraron órdenes de compra."));
+            echo json_encode(["status"=>"success", $itemRecords]);
+
+        } else {
+            ["status"=>"error", "message" => "No hay órdenes de compra para las fechas especificadas."]);
+        }
+    } catch (Exception $e) {
+        echo json_encode(["status"=>"error","message" => $e->getMessage()])
     }
 }
