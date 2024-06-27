1a1c4cb4f74305bc6306930343384fd555729a78
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-06-27 10:06:17 -0600

    Catálogo de remisiones

---
+++ remisiones/leer.php
@@ -0,0 +1,37 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"]=="GET")
+{
+    require_once '../conexion.php';
+    include '../config.php';
+
+    $query="SELECT r.id, c.codigo, r.total_pares, r.precio_final
+	    FROM remisiones r
+	    INNER JOIN clientes c ON r.cliente_id = c.id
+	    ORDER BY r.fecha DESC;";
+
+    $resultado=$mysql->query($query);
+    if($resultado->num_rows > 0)
+    {
+        $itemRecords=array();
+        $itemRecords["items"]=array();
+        while ($item = $resultado->fetch_assoc())
+        {
+            extract($item);
+
+            $itemDetails=array(
+                "id" => $id,
+                "codigo" => $codigo,
+                "total_pares" => $total_pares,
+                "precio_final" => $precio_final,
+            );
+            array_push($itemRecords["items"], $itemDetails);
+ }
+        http_response_code(200);
+        echo json_encode($itemRecords);
+ } else {
+        http_response_code(404);
+        echo json_encode(array("message" => "No se encontraron remisiones."));
+  }
+}
+?>
