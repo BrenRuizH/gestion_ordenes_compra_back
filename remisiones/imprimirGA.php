fa3601cbedc8c46ac63e8d930741710b36b24fe9
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-07-12 14:10:19 -0600

    Update imprimirGA.php

--- remisiones/imprimirGA.php
+++ remisiones/imprimirGA.php
@@ -11,22 +11,15 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         SELECT c.razonSocial, c.direccion, c.telefono, c.id AS cliente,
                r.id AS remision, r.extra, r.descripcion, r.oc,
                h.id AS horma_id, h.nombre AS nombre_horma, h.precio AS precio_horma,
-               rpc.punto, rpc.cantidad, 
-               total_suma.total_pares
+               rpc.punto, rpc.cantidad
         FROM remisiones r
         INNER JOIN clientes c ON r.cliente_id = c.id
         INNER JOIN remision_puntos_cantidades rpc ON r.id = rpc.remision_id
         INNER JOIN hormas h ON h.id = rpc.horma_id
-        INNER JOIN (
-          SELECT remision_id, SUM(cantidad) AS total_pares
-          FROM remision_puntos_cantidades
-          WHERE remision_id = ?
-          GROUP BY remision_id
-        ) AS total_suma ON r.id = total_suma.remision_id
         WHERE r.id = ?;";
     
     $stmt = $mysql->prepare($query);
-    $stmt->bind_param("ii", $remision_id, $remision_id);
+    $stmt->bind_param("i", $remision_id);
     $stmt->execute();
     $resultado = $stmt->get_result();
 
@@ -61,21 +54,25 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
             if (!isset($response['orden_compra'][$horma_id])) {
                 $response['orden_compra'][$horma_id] = [
                     'id' => $horma_id,
-                    'total_pares' => $row['total_pares'],
+                    'total_pares' => 0,
                     'horma' => [
                         'id' => $row['horma_id'],
                         'nombre' => $row['nombre_horma'],
                         'precio' => $row['precio_horma']
                     ],
-                    'detalles' => []
+                    'detalles' => [],
                 ];
             }
 
             // Añadir detalle de orden de compra a la horma correspondiente
+            $cantidad = $row['cantidad'];
             $response['orden_compra'][$horma_id]['detalles'][] = [
                 'punto' => $row['punto'],
-                'cantidad' => $row['cantidad']
+                'cantidad' => $cantidad
             ];
+
+            // Sumar cantidad al total_pares de la horma correspondiente
+            $response['orden_compra'][$horma_id]['total_pares'] += $cantidad;
         }
 
         // Convertir arrays a valores para asegurar la estructura correcta en JSON
