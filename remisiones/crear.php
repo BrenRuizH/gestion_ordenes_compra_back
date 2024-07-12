0b761839f83bf65280dff222eea3c797d9bdf720
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-07-11 22:37:17 -0600

    Update crear.php

--- remisiones/crear.php
+++ remisiones/crear.php
@@ -7,8 +7,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cliente_id = $_POST['cliente_id'];
     $total_pares = $_POST['total_pares'];
     $precio_final = $_POST['precio_final'];
-    $extra = isset($_POST['extra']) ? "'".$_POST['extra']."'" : 'NULL';
-    $descripcion = isset($_POST['descripcion']) ? "'".$_POST['descripcion']."'" : 'NULL';
+    $extra = $_POST['extra'];
+    $descripcion = $_POST['descripcion'];
     $folio = $_POST['folio'];
 
     $elementosAgregados = isset($_POST['elementosAgregados']) ? json_decode($_POST['elementosAgregados'], true) : [];
