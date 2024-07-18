0810e0f9ec9fc489ed8fdcc5f1a2fa59fb1f2d2b
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-07-18 01:02:56 -0600

    Actualizar servicio de editar para Gallery

--- remisiones/editar.php
+++ remisiones/editar.php
@@ -12,11 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $folios = json_decode($_POST['folios'], true);
     $extra = $_POST['extra'];
     $descripcion = $_POST['descripcion'];
-    
+    $elementosAgregados = isset($_POST['elementosAgregados']) ? json_decode($_POST['elementosAgregados'], true) : [];
 
     try {
         $mysql->begin_transaction();
-
         $stmt = $mysql->prepare("CALL EditarRemisionYDetalles(?, ?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("isiidds", $id, $fecha, $cliente_id, $total_pares, $precio_final, $extra, $descripcion);
         if (!$stmt->execute()) {
@@ -33,19 +32,36 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 throw new Exception("Error al insertar detalle de remisión: " . $stmt->error);
             }
             
-            $stmt= $mysql ->prepare ("UPDATE ordenes_compra SET status = 'REMISIONADO', remision_id = ? WHERE folio = ?");
+            $stmt = $mysql->prepare("UPDATE ordenes_compra SET status = 'REMISIONADO', remision_id = ? WHERE folio = ?");
             $stmt->bind_param("is", $id, $folio);
             if (!$stmt->execute()) {
                 throw new Exception("Error al actualizar el status: " . $stmt->error);
             }
         }
+
+        if ($cliente_id == 36) {
+            foreach ($elementosAgregados as $elemento) {
+                $horma_id = $elemento['horma_id'];
+                $oc = $elemento['oc'];
+                foreach ($elemento['puntosYcantidades'] as $puntoCantidad) {
+                    $punto = $puntoCantidad['punto'];
+                    $cantidad = $puntoCantidad['cantidad'];
+                    $stmt = $mysql->prepare("INSERT INTO remision_puntos_cantidades (remision_id, horma_id, punto, cantidad, oc) VALUES (?, ?, ?, ?, ?)");
+                    $stmt->bind_param("iidis", $id, $horma_id, $punto, $cantidad, $oc);
+                    if (!$stmt->execute()) {
+                        throw new Exception("Error al insertar detalle de horma: " . $stmt->error);
+                    }
+                }
+            }
+        }
         
         $mysql->commit();
 
-        echo json_encode(["status"=>"success","message" => "Remisión editada exitosamente"]);
+        echo json_encode(["status" => "success", "message" => "Remisión editada exitosamente"]);
     } catch (Exception $e) {
         $mysql->rollback();
 
-        echo json_encode(["status"=>"error","message" => $e->getMessage()]);
+        echo json_encode(["status" => "error", "message" => $e->getMessage()]);
     }
 }
+?>
