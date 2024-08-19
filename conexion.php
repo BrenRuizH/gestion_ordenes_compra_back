9dc1a3ba30bd43e79685f20309be7ae4e27c759b
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-08-19 16:50:06 -0600

    pruebas

--- conexion.php
+++ conexion.php
@@ -1,6 +1,9 @@
 <?php
 
-$mysql = new mysqli("o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ie5g1p0as6ry8me4", "cd1w2cpo2o5c94fa", "xj9i40s68gou6ok7");
+// HOSTNAME / USERNAME / PASSWORD / BD
+//$mysql = new mysqli("o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ie5g1p0as6ry8me4", "cd1w2cpo2o5c94fa", "xj9i40s68gou6ok7");
+
+$mysql = new mysqli("localhost", "root", "root", "prueba");
 
 if ($mysql->connect_error) {
     die("Error de conexión");
