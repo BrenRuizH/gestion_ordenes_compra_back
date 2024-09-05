2991cdec90dde1f436a2f6367f78a4d93a181600
Brenda Ruiz <145076497+BrenRuizH@users.noreply.github.com>
2024-09-04 23:14:59 -0600

    Update editar.php

--- remisiones/editar.php
+++ remisiones/editar.php
@@ -9,9 +9,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cliente_id = $_POST['cliente_id'];
     $total_pares = $_POST['total_pares'];
     $precio_final = $_POST['precio_final'];
-    $folios = json_decode($_POST['folios'], true);
     $extra = $_POST['extra'];
     $descripcion = $_POST['descripcion'];
+    $folios = json_decode($_POST['folios'], true);
+
     $elementosAgregados = isset($_POST['elementosAgregados']) ? json_decode($_POST['elementosAgregados'], true) : [];
 
     try {
